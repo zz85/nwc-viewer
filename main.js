cca20839bc8f15096b0491f65ce14434c0110480
Joshua Koo <zz85nus@gmail.com>
2015-09-14 03:46:51 +0800

    Inflate not Rawinflate

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ function ajax(url, callback) {
 
 var byteArray;
 
-ajax('samples/anongs.nwc', process);
+ajax('samples/anongs.nwc', received);
 
 function hex(number) {
 	return ('00' + (number || 0).toString(16)).slice(-2);
@@ -28,10 +28,7 @@ function string(number) {
 	return (' ' + String.fromCharCode(number)).slice(-1);
 }
 
-function process(arrayBuffer) {
-	byteArray = new Uint8Array(arrayBuffer);
-	console.log('byteArray', byteArray);
-
+function dump(byteArray) {
 	for (var i = 0; i < byteArray.length; i+=4) {
 		console.log(
 			hex(byteArray[i]),
@@ -39,15 +36,30 @@ function process(arrayBuffer) {
 			hex(byteArray[i + 2]),
 			hex(byteArray[i + 3]),
 
-			string(byteArray[i]),
-			string(byteArray[i + 1]),
-			string(byteArray[i + 2]),
-			string(byteArray[i + 3]),
-
 			binary(byteArray[i]),
 			binary(byteArray[i + 1]),
 			binary(byteArray[i + 2]),
-			binary(byteArray[i + 3])
+			binary(byteArray[i + 3]),
+
+			string(byteArray[i]),
+			string(byteArray[i + 1]),
+			string(byteArray[i + 2]),
+			string(byteArray[i + 3])
 		);
 	}
+
+}
+
+function received(arrayBuffer) {
+	byteArray = new Uint8Array(arrayBuffer);
+	console.log('byteArray', byteArray);
+
+	var nwz = byteArray.subarray(6);
+	var inflate = new Zlib.Inflate(nwz);
+	var plain = inflate.decompress();
+	process(plain);
+}
+
+function process(bytes) {
+	dump(bytes);
 }
