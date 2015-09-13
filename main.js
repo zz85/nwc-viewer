2d68d7e7e335a76ddf1c7ad6492b0d84682d76d7
Joshua Koo <zz85nus@gmail.com>
2015-09-14 03:30:59 +0800

    Initial Hex Debugging

--- main.js
+++ main.js
@@ -6,13 +6,48 @@ function ajax(url, callback) {
 	oReq.onload = function(oEvent) {
 		console.log('ajax done for ', url);
 		var arrayBuffer = oReq.response;
-		var byteArray = new Uint8Array(arrayBuffer);
-		callback(byteArray);
+		callback(arrayBuffer);
 	};
 
 	oReq.send();
 }
 
-ajax('samples/anongs.nwc', function(buffers) {
-	console.log('buffers', buffers);
-});
+var byteArray;
+
+ajax('samples/anongs.nwc', process);
+
+function hex(number) {
+	return ('00' + (number || 0).toString(16)).slice(-2);
+}
+
+function binary(number) {
+	return ('00000000' + (number || 0).toString(2)).slice(-8);
+}
+
+function string(number) {
+	return (' ' + String.fromCharCode(number)).slice(-1);
+}
+
+function process(arrayBuffer) {
+	byteArray = new Uint8Array(arrayBuffer);
+	console.log('byteArray', byteArray);
+
+	for (var i = 0; i < byteArray.length; i+=4) {
+		console.log(
+			hex(byteArray[i]),
+			hex(byteArray[i + 1]),
+			hex(byteArray[i + 2]),
+			hex(byteArray[i + 3]),
+
+			string(byteArray[i]),
+			string(byteArray[i + 1]),
+			string(byteArray[i + 2]),
+			string(byteArray[i + 3]),
+
+			binary(byteArray[i]),
+			binary(byteArray[i + 1]),
+			binary(byteArray[i + 2]),
+			binary(byteArray[i + 3])
+		);
+	}
+}
