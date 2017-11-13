f6e60caab1919603d6e050b1e4b3ed5d78aef5cd
Joshua Koo <zz85nus@gmail.com>
2017-11-14 07:52:59 +0800

    Adding glyphs
    - time signatures
    - accidentals
    - notehead
    - flags

--- src/drawing.js
+++ src/drawing.js
@@ -1,5 +1,61 @@
 fontMap = {
-	treble: 'e050'
+	// barlines
+
+	// clefs
+	gClef: 'e050', // treble
+	cClef: 'e05c', // alto
+	fClef: 'e062', // bass
+
+	// time signatures
+	timeSig0: 'e080',
+	timeSig1: 'e081',
+	timeSig2: 'e082',
+	timeSig3: 'e083',
+	timeSig4: 'e084',
+	timeSig5: 'e085',
+	timeSig6: 'e086',
+	timeSig7: 'e087',
+	timeSig8: 'e088',
+	timeSig9: 'e089',
+	timeSigCommon: 'e08a',
+
+	// text based only
+	// timeSigCombNumerator timeSig8Numerator
+	// timeSigCombNumerator: 'E09E',
+
+	// stemUpSE
+
+	// Noteheads
+	noteheadDoubleWhole: 'E0A0',
+	noteheadWhole: 'E0A2',
+	noteheadHalf: 'E0A3',
+	noteheadBlack: 'E0A4',
+
+	noteWhole: 'E1D2', // 1D15D
+	noteHalfUp: 'E1D3', // 1D15E
+	
+	// noteheadHalfFilled: ''
+	// 1D15E = 157 165
+	// 	U+1D165 
+
+	flag8thUp: 'E240',
+	flag8thDown: 'E241',
+	flag16thUp: 'E242',
+	flag16thDown: 'E243',
+	flagInternalUp: 'E250',
+	flagInternalDown: 'E251',
+
+	// Standard accidentals (12-EDO) (U+E260–U+E26F)
+	
+	accidentalFlat: 'e260',
+	accidentalNatural: 'e261',
+	accidentalSharp: 'e262',
+	accidentalDoubleSharp: 'e263',
+
+	textBlackNoteShortStem: 'E1F0',
+	textAugmentationDot: 'E1FC',
+	textTuplet3ShortStem: 'E1FF',
+	
 }
 
 getCode = (name) => String.fromCharCode
@@ -31,24 +87,23 @@ class Draw {
 
 	positionY(semitones) {
 		this.offsetY = -semitones / 2 / 4 * 40
-		console.log('this.offsetY', this.offsetY)
 	}
 }
 
 class Stave extends Draw {
-	constructor() {
+	constructor(width) {
 		super()
 		this.size = 40  // TODO global
 		this.x = 0
 		this.y = 0
-		this.width = 100
+		this.width = width || 100
 	}
 
 	draw(ctx) {
 		const {x, y, width, size} = this
 
 		ctx.strokeStyle = '#000'
-		ctx.lineWidth = 1.4
+		ctx.lineWidth = 1.3
 
 		// 5 lines
 		const spaces = 4; // TODO global
@@ -64,26 +119,67 @@ class Stave extends Draw {
 	}
 }
 
-class Treble extends Draw {
-	constructor() {
+class Glyph extends Draw {
+	constructor(char, adjustY) {
 		super()
 
-		this.char = getCode('treble')
+		this.name = char
+		this.char = getCode(char)
 		// TODO remove ctx hardcoding
 		this.width = ctx.measureText(this.char).width
+		this.offsetX = this.width;
+		if (adjustY) this.positionY(adjustY)
 	}
 
 	draw(ctx) {
 		ctx.fillStyle = '#000'
 		ctx.fillText(this.char, 0, 0)
 
-		this.debug(ctx);
+		// this.debug(ctx);
 	}
 }
 
-class Drawing {
+const Clef = Glyph
+
+/**
+ * Clefs
+ */
+
+class TrebleClef extends Clef {
 	constructor() {
+		super('gClef', 2)
+	}
+}
+
+class BassClef extends Clef {
+	constructor() {
+		super('fClef', 6)
+	}
+}
+
+class AltoClef extends Clef {
+	constructor() {
+		super('cClef', 4)
+	}
+}
+
+/**
+ * Time signatures
+ */
+class TimeSignature extends Glyph {
+	constructor(x=0, y) {
+		super('timeSig' + x, y)
+	}
+}
+
+
+class Drawing {
+	constructor(ctx) {
 		this.set = new Set()
+
+		ctx.font = '40px Bravura'
+		ctx.textBaseline = 'alphabetic' // alphabetic  bottom top
+
 	}
 
 	add(el) {
@@ -95,11 +191,11 @@ class Drawing {
 	}
 
 	draw(ctx) {
+		ctx.save()
 		for (const el of this.set) {
 			if (el instanceof Draw) {
 				ctx.save()
 				ctx.translate(el.x, el.y)
-				console.log('el.offsetY', el.offsetX || 0, el.offsetY || 0);
 				ctx.translate(el.offsetX || 0, el.offsetY || 0)
 				el.draw(ctx)
 				ctx.restore()
@@ -108,6 +204,7 @@ class Drawing {
 				console.log('Element', el, 'not a draw element')
 			}
 		}
+		ctx.restore()
 	}
 }
 
